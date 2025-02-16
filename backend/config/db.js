50460119ef1989a621f55629fe861ffbdbab4667
Skpanda0 <skpanda1009@gmail.com>
2025-02-16 22:04:04 +0530

    server code changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,30 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 
-const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{useUnifiedTopology :true ,useNewUrlParser: true})
-        console.log('MongoDB connected')
-    }catch(err)
-    {
-        console.log(err.message)
-        process.exit(1)
-    }
-}
-module.exports = connectDB
+// const connectDB = async ()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGO_URI,{useUnifiedTopology :true ,useNewUrlParser: true})
+//         console.log('MongoDB connected')
+//     }catch(err)
+//     {
+//         console.log(err.message)
+//         process.exit(1)
+//     }
+// }
+// module.exports = connectDB
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('✅ MongoDB Connected');
+  } catch (err) {
+    console.error('❌ MongoDB Connection Error:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
